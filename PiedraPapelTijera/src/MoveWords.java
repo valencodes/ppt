fc578babdf152422e9a3e91c3721866e78cb272a
Valentina <valentinagaleote@gmail.com>
2023-02-08 17:23:10 +0100

    Realizada la ampliación del juego con las nuevas reglas

--- PiedraPapelTijera/src/MoveWords.java
+++ PiedraPapelTijera/src/MoveWords.java
@@ -6,7 +6,7 @@ public class MoveWords{
     public static final int GANA = 1;
     public static final int PIERDE = 2;
 
-    private static final String[] validMoves = {"TIJERAS", "PAPEL", "PIEDRA"};
+    private static final String[] validMoves = {"TIJERAS", "PAPEL", "PIEDRA", "LAGARTIJA", "SPOCK"};
     private static final String[] validCommands = {"SALIR", "HELP"};
 
     private Random rnd;
@@ -64,10 +64,31 @@ public class MoveWords{
 
 	    first_i = getIndex(first);
 	    second_i = getIndex(second);
-
-	    if (first_i == second_i) return EMPATE;
 	    
-	    return (( (first_i +1) % validMoves.length ) == second_i ) ? GANA: PIERDE;
+	    //"TIJERAS" = 0 , "PAPEL" = 1, "PIEDRA" = 2, "LAGARTIJA" = 3, "SPOCK" = 4
+	    
+	    if (first_i == 0 & (second_i == 1 || second_i == 3)) {
+	    	return GANA;
+	    } else if (first_i == 1 && (second_i == 2 || second_i == 4)) {
+	    	return GANA;
+	    } else if (first_i == 2 && (second_i == 0 ||  second_i == 0)) {
+	    	return GANA;
+	    } else if (first_i == 3 && (second_i ==4 || second_i == 1)) {
+	    	return GANA;
+	    } else if (first_i == 4 && (second_i  == 0 || second_i == 2)) {
+	    	return GANA;
+	    } else if (first_i == second_i) {
+	    	return EMPATE;
+	    } else {
+	    	return PIERDE;
+	    }
+	    
+	}
+
+
+	    //if (first_i == second_i) return EMPATE;
+	    
+	   // return (( (first_i +1) % validMoves.length ) == second_i ) ? GANA: PIERDE;
 	}
 	
-} 
+ 
